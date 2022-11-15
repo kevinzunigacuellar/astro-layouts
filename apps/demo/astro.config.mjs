c2863558697119f25f4d633857272edfaf13b501
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-15 12:04:59 -0500

    added mdx files and subfolder example

--- apps/demo/astro.config.mjs
+++ apps/demo/astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import plugin from 'astro-layouts'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [[plugin, {
       default: '/src/layouts/RedLayout.astro',
-      blog: '/src/layouts/BlueLayout.astro'
+      blog: '@layouts/BlueLayout.astro',
+      'blog/bar': '@layouts/PinkLayout.astro',
     }]],
     extendDefaultPlugins: true,
-  }
+  },
+  integrations: [mdx()]
 });
