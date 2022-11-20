c4488cece28cee7ab88fddf2b94705d0f88b99fb
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-20 14:30:35 -0500

    rename variables names

--- packages/astro-layouts/test/folder.test.ts
+++ packages/astro-layouts/test/folder.test.ts
@@ -1,9 +1,9 @@
 import { test, expect } from "vitest";
 import { renderMarkdown } from "@astrojs/markdown-remark";
 import type { LayoutConfig, Metadata } from "../src/types";
-import plugin from "../index";
+import astroLayouts from "../index";
 
-const options : LayoutConfig = {
+const layoutOptions : LayoutConfig = {
   folder: "posts",
   default: "/src/layouts/Base.astro",
 };
@@ -12,8 +12,8 @@ test("adds Base.astro layout to all files in 'src/posts'", async () => {
   const fileURL = new URL("posts/index.md", import.meta.url);
   const result = await renderMarkdown("# Hello, world!", {
     fileURL,
-    remarkPlugins: [[plugin, options]],
+    remarkPlugins: [[astroLayouts, layoutOptions]],
   });
   const pageMetadata = result.vfile.data.astro as Metadata;
-  expect(pageMetadata.frontmatter.layout).toBe(options.default);
+  expect(pageMetadata.frontmatter.layout).toBe(layoutOptions.default);
 });
--- packages/astro-layouts/test/layouts.test.ts
+++ packages/astro-layouts/test/layouts.test.ts
@@ -1,40 +1,40 @@
 import { test, expect } from "vitest";
 import { renderMarkdown } from "@astrojs/markdown-remark";
 import type { LayoutConfig, Metadata } from "../src/types";
-import plugin from "../index";
+import astroLayouts from "../index";
 
-const options : LayoutConfig = {
+const layoutOptions : LayoutConfig = {
   default: "/src/layouts/Base.astro",
   blog: "/src/layouts/BlogLayout.astro",
   "blog/foo": "/src/layouts/Custom.astro",
 };
 
-test("adds Base.astro layout to all files", async () => {
+test("adds Base.astro layout to all files in 'src/pages'", async () => {
   const fileURL = new URL("pages/index.md", import.meta.url);
   const result = await renderMarkdown("# Hello, world!", {
     fileURL,
-    remarkPlugins: [[plugin, options]],
+    remarkPlugins: [[astroLayouts, layoutOptions]],
   });
   const pageMetadata = result.vfile.data.astro as Metadata;
-  expect(pageMetadata.frontmatter.layout).toBe(options.default);
+  expect(pageMetadata.frontmatter.layout).toBe(layoutOptions.default);
 });
 
-test("adds BlogLayout.astro layout to 'pages/blog'", async () => {
+test("adds BlogLayout.astro layout to 'src/pages/blog'", async () => {
   const fileURL = new URL("pages/blog/index.md", import.meta.url);
   const result = await renderMarkdown("# Hello, world!", {
     fileURL,
-    remarkPlugins: [[plugin, options]],
+    remarkPlugins: [[astroLayouts, layoutOptions]],
   });
   const pageMetadata = result.vfile.data.astro as Metadata;
-  expect(pageMetadata.frontmatter.layout).toBe(options.blog);
+  expect(pageMetadata.frontmatter.layout).toBe(layoutOptions.blog);
 });
 
-test("adds Custom.astro layout to 'pages/blog/foo'", async () => {
+test("adds Custom.astro layout to 'src/pages/blog/foo'", async () => {
   const fileURL = new URL("pages/blog/foo/index.md", import.meta.url);
   const result = await renderMarkdown("# Hello, world!", {
     fileURL,
-    remarkPlugins: [[plugin, options]],
+    remarkPlugins: [[astroLayouts, layoutOptions]],
   });
   const pageMetadata = result.vfile.data.astro as Metadata;
-  expect(pageMetadata.frontmatter.layout).toBe(options["blog/foo"]);
+  expect(pageMetadata.frontmatter.layout).toBe(layoutOptions["blog/foo"]);
 });
