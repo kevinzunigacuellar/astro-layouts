1b51ce13076f9cea34f3eaf9782d49ad09aa4b93
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-22 13:46:09 -0500

    replace micromatch with picomatch

--- packages/astro-layouts/src/remarkPlugin.ts
+++ packages/astro-layouts/src/remarkPlugin.ts
@@ -1,12 +1,12 @@
 import type { RemarkPlugin } from "@astrojs/markdown-remark";
-import micromatch from "micromatch";
+import picomatch from "picomatch";
 
 export const astroLayout: RemarkPlugin = (options: Record<string, string>) => {
   return function (_tree, file) {
     const [filePath] = file.history;
     const path = filePath.replace(/.*src\//, "");
     for (const [glob, layoutPath] of Object.entries(options)) {
-      if (micromatch.isMatch(path, glob)) {
+      if (picomatch.isMatch(path, glob)) {
         const metadata = file.data.astro as { frontmatter: { layout: string } };
         metadata.frontmatter.layout = layoutPath;
       }
